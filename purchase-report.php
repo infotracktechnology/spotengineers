e1f49220589ecdccd43990ec1d2efb2340fd5ecd
Jeeva7028 <nanthankjeeva@gmail.com>
2024-09-28 11:14:28 +0530

    ok

--- purchase-report.php
+++ purchase-report.php
@@ -25,8 +25,8 @@ $fromDate = date('Y-m-01');
     <meta charset="UTF-8" />
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport" />
     <title>Purchase Register</title>
-   <!-- General CSS Files -->
-   <link rel="stylesheet" href="assets/css/app.min.css" />
+     <!-- General CSS Files -->
+     <link rel="stylesheet" href="assets/css/app.min.css" />
     <link rel="stylesheet" href="assets/bundles/summernote/summernote-bs4.css" />
     <link rel="stylesheet" href="assets/bundles/jquery-selectric/selectric.css" />
     <link rel="stylesheet" href="assets/bundles/bootstrap-tagsinput/dist/bootstrap-tagsinput.css" />
@@ -35,6 +35,10 @@ $fromDate = date('Y-m-01');
     <link rel="stylesheet" href="assets/css/components.css" />
     <!-- Custom style CSS -->
     <link rel="stylesheet" href="assets/css/custom.css" />
+    <link rel="stylesheet" href="assets/bundles/datatables/datatables.min.css" />
+    <link rel="stylesheet" href="assets/bundles/datatables/DataTables-1.10.16/css/dataTables.bootstrap4.min.css" />
+    <link rel="stylesheet" href="assets/bundles/select2/dist/css/select2.min.css" />
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico" />
     <link href="https://cdn.jsdelivr.net/npm/tom-select@2.3.1/dist/css/tom-select.css" rel="stylesheet">
     <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico" />
     <script src="//unpkg.com/alpinejs" defer></script>
@@ -51,68 +55,89 @@ $fromDate = date('Y-m-01');
                     <div class="section-body">
                         <div class="row">
                             <div class="col-md-12">
-                                <form method="post" name="myForm" action="purchase_report.php" enctype="multipart/form-data">
-                                    <div class="card card-primary">
-                                        <div class="card-header">
-                                            <h4 class="col-deep-purple m-0">Purchase Register</h4>
-                                        </div>
-                                        <div class="card-body">
-                                            <div class="row">
-                                               
-                                            <div class="col-md-2 form-group">
-                                                <label class="col-blue">From Date</label>
-                                                <input type="date" name="from_date" class="form-control form-control-sm" value="<?php echo $fromDate; ?>" />
-                                            </div>
+                            <form method="post" name="myForm" action="purchase-report.php" enctype="multipart/form-data">
+    <div class="card card-primary">
+        <div class="card-header">
+            <h4 class="col-deep-purple m-0">Purchase Register</h4>
+        </div>
+        <div class="card-body">
+            <div class="row">
 
-                                            <div class="col-md-2 form-group">
-                                                <label class="col-blue">To Date</label>
-                                                <input type="date" name="to_date" class="form-control form-control-sm" value="<?php echo $currentDate; ?>" />
-                                            </div>
+                <div class="col-md-2 form-group">
+                    <label class="col-blue">From Date</label>
+                    <input type="date" name="from_date" class="form-control form-control-sm" value="<?php echo $fromDate; ?>" />
+                </div>
 
-                                            <div class="col-md-4 form-group">
-                                                    <label class="col-blue">Supplier </label>
-                                                    <select name="supplier" id="supplier"  required>
-                                                        <option value="">Select Supplier</option>
-                                                        <?php foreach ($suppliers as $row) { ?>
-                                                            <option value="<?php echo $row->supplier_id; ?>"><?php echo $row->supplier_name; ?></option>
-                                                        <?php } ?>
-                                                    </select>
-                                                </div>
-                                        
-                                            <div class="col-md-2 form-group">
-                                                <button type="submit" class="btn btn-success" style="margin-top: 25px;">Submit</button>
-                                            </div>
-                                        </div>
+                <div class="col-md-2 form-group">
+                    <label class="col-blue">To Date</label>
+                    <input type="date" name="to_date" class="form-control form-control-sm" value="<?php echo $currentDate; ?>" />
+                </div>
 
-                                    </div>
-                                  
-                        <?php if($_SERVER["REQUEST_METHOD"] == "POST"){ 
-                          
-                          $from_date = $_POST['from_date'];
-                          $to_date = $_POST['to_date'];
-                          
-                          ?>
+                <div class="col-md-4 form-group">
+                    <label class="col-blue">Supplier</label>
+                    <select name="supplier" id="supplier" required>
+                        <option value="">Select Supplier</option>
+                        <option value="all">All Suppliers</option>
+                        <?php foreach ($suppliers as $row) { ?>
+                            <option value="<?php echo $row->supplier_id; ?>"><?php echo $row->supplier_name; ?></option>
+                        <?php } ?>
+                    </select>
+                </div>
 
-                                    <div class="card-body">
-                                    <div class="table-responsive">
-  <table class="table table-striped table-hover" id="tableExport" style="width:100%;">
-    <thead>
-      <tr>
-       
-      </tr>
-    </thead>
-    <tbody>
-    
-    </tbody>
-  </table>
-</div>
+                <div class="col-md-2 form-group">
+                    <button type="submit" class="btn btn-success" style="margin-top: 25px;">Submit</button>
+                </div>
+            </div>
+        </div>
 
-                  </div>
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $from_date = $_POST['from_date'];
+            $to_date = $_POST['to_date'];
+            $supplier = $_POST['supplier'];
+            $where = $_POST['supplier'] == 'all' ? '' : "and b.supplier_id = '$supplier'";
+            
+            $sql = "SELECT a.receipt_no, a.receipt_date, b.supplier_name, b.city, a.invoice_no, a.invoice_date, a.grand_total FROM purchase a INNER JOIN suppliers b ON a.purchase_id = b.supplier_id WHERE a.receipt_date BETWEEN '$from_date' AND '$to_date' $where";
 
-                  <?php } ?>
+            $result1 = $con->query($sql);
+            $purchase = $result1->fetch_all(MYSQLI_ASSOC);
+        ?>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-striped table-hover" id="tableExport" style="width:100%;">
+                        <thead>
+                            <tr>
+                                <th>S.No</th>
+                                <th>Receipt No</th>
+                                <th>Receipt Date</th>
+                                <th>Supplier Name</th>
+                                <th>City</th>
+                                <th>Invoice No</th>
+                                <th>Invoice Date</th>
+                                <th>Total Amount</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($purchase as $key => $purchases) : ?>
+                                <tr>
+                                    <td><?php echo $key + 1; ?></td>
+                                    <td><?php echo $purchases['receipt_no']; ?></td>
+                                    <td><?php echo $purchases['receipt_date']; ?></td>
+                                    <td><?php echo $purchases['supplier_name']; ?></td>
+                                    <td><?php echo $purchases['city']; ?></td>
+                                    <td><?php echo $purchases['invoice_no']; ?></td>
+                                    <td><?php echo $purchases['invoice_date']; ?></td>
+                                    <td><?php echo $purchases['grand_total']; ?></td>
+                                </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        <?php } ?>
+    </div>
+</form>
 
-                            </div>
-                            </form>
                         </div>
                     </div>
             </div>
@@ -123,8 +148,8 @@ $fromDate = date('Y-m-01');
     <!-- General JS Scripts -->
     <script src="assets/js/app.min.js"></script>
   <!-- JS Libraies -->
-  <!-- Page Specific JS File -->
-  <script src="assets/bundles/datatables/datatables.min.js"></script>
+ <!-- Page Specific JS File -->
+ <script src="assets/bundles/datatables/datatables.min.js"></script>
   <script src="assets/bundles/datatables/DataTables-1.10.16/js/dataTables.bootstrap4.min.js"></script>
   <script src="assets/bundles/datatables/export-tables/dataTables.buttons.min.js"></script>
   <script src="assets/bundles/datatables/export-tables/buttons.flash.min.js"></script>
