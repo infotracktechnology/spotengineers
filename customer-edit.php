9d3447460f5886d34b80ab9eca3b42c70e251f8d
infotracktechnology <infotracktechnologysolutions@gmail.com>
2024-10-04 15:35:18 +0530

    update

--- customer-edit.php
+++ customer-edit.php
@@ -16,6 +16,8 @@ if ($result) {
     $customer[] = $row;
   }
 }
+$customer_appliances = $con->query("SELECT * FROM customer_appliances WHERE customer_id = '$id'")->fetch_all(MYSQLI_ASSOC);
+$customer_appliances = json_encode($customer_appliances,JSON_UNESCAPED_UNICODE);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -46,7 +48,7 @@ if ($result) {
     <div class="main-wrapper main-wrapper-1">
       <?php require('sidebar.php'); ?>
       <!-- Main Content -->
-      <div class="main-content">
+      <div class="main-content" x-data="app">
         <section class="section">
           <div class="section-body">
             <div class="row">
@@ -56,10 +58,11 @@ if ($result) {
                     <h4 class="col-deep-purple m-0">Customer</h4>
                   </div>
                   <div class="card-body">
-                  <form method="post" id="myForm" action="customer-update.php?id=<?= $id; ?>" enctype="multipart/form-data" x-data="{ customerType: '<?= $customer[0]->type; ?>' }">
+                  <form method="post" id="myForm" action="customer-update.php" enctype="multipart/form-data" x-data="{ customerType: '<?= $customer[0]->type; ?>' }">
                        <div class="row">
                            <div class="col-md-3 form-group">
                                <label class="col-blue">Name</label>
+                               <input type="hidden" name="id" value="<?= $customer[0]->id; ?>">
                                <input type="text" name="name" id="name" class="form-control form-control-sm" value="<?= $customer[0]->name; ?>" required>
                            </div>
 
@@ -104,13 +107,15 @@ if ($result) {
 </div>
 
 <div class="col-md-12 form-group mt-0">
-    <button type="button" class="btn btn-warning" id="addItemBtn"><i class="fa fa-plus"></i> Add Item</button>
+    <button type="button" class="btn btn-warning" @click="addAppliance" id="addItemBtn"><i class="fa fa-plus"></i> Add Item</button>
 </div>
-<div id="itemContainer" class="col-md-12"></div>
-<div class="col-md-12 row item-row" id="initialItemRow">
-    <div class="col-md-4 form-group">
-        <label class="col-blue">Appliance List</label>
-        <select name="appliance" class="form-control form-control-sm" value="<?= $customer[0]->appliance; ?>" required>
+
+<div class="col-md-12" id="itemContainer">
+    <template x-for="(appliance, index) in appliances">
+      <div class="row item-row" id="initialItemRow">
+        <div class="col-md-4 form-group">
+          <label class="col-blue">Appliance List</label>
+          <select name="appliance[]" class="form-control form-control-sm" x-model="appliance.appliance" required>
             <option value="">Select Appliance</option>
             <option value="Air Conditioner">Air Conditioner</option>
             <option value="Deep Freezer">Deep Freezer</option>
@@ -120,21 +125,25 @@ if ($result) {
             <option value="Water Heater">Water Heater</option>
             <option value="UPS">UPS</option>
             <option value="Dish">Dish (DTH)</option>
-        </select>
-    </div>
-    <div class="col-md-3 form-group">
-        <label class="col-blue">Brand</label>
-        <input type="text" name="brand" value="<?= $customer[0]->brand; ?>"  class="form-control form-control-sm" required>
-    </div>
-    <div class="col-md-3 form-group">
-        <label class="col-blue">Appliance Name</label>
-        <input type="text" name="appliance_name" value="<?= $customer[0]->appliance_name; ?>"  class="form-control form-control-sm" required>
-    </div>
-    
-    <div class="col-md-1 form-group">
-        <button type="button" class="btn btn-danger mt-4 removeItemBtn"><i class="fa fa-times"></i></button>
-    </div>
-</div>
+          </select>
+        </div>
+        <div class="col-md-3 form-group">
+          <label class="col-blue">Brand</label>
+          <input type="text" x-model="appliance.brand" name="brand[]" class="form-control form-control-sm" required>
+        </div>
+        <div class="col-md-3 form-group">
+          <label class="col-blue">Appliance Name</label>
+          <input type="text" x-model="appliance.appliance_name" name="appliance_name[]" class="form-control form-control-sm" required>
+        </div>
+        <div class="col-md-1 form-group">
+          <button type="button" class="btn btn-danger mt-4" @click="removeAppliance(index)">
+            <i class="fa fa-times"></i>
+          </button>
+        </div>
+      </div>
+    </template>
+  </div>
+
 
 <div class="col-md-12">
     <button type="submit" class="btn btn-success">Submit</button>
@@ -161,33 +170,24 @@ if ($result) {
     <script src="assets/js/app.js"></script>
 </body>
 <script>
-
-document.addEventListener('DOMContentLoaded', function () {
-    const addItemBtn = document.getElementById('addItemBtn');
-    const itemContainer = document.getElementById('itemContainer');
-    const initialItemRow = document.getElementById('initialItemRow');
-
- 
-    function addRemoveButtonListener(row) {
-        const removeBtn = row.querySelector('.removeItemBtn');
-        removeBtn.addEventListener('click', function () {
-            if (confirm('Are you sure you want to delete this item?')) {
-                row.remove();
-            }
-        });
+document.addEventListener('alpine:init', () => {
+  Alpine.data('app', () => ({
+    appliances: JSON.parse(`<?= $customer_appliances ?>`),
+    addAppliance() {
+      this.appliances.push({
+        id: '',
+        brand: '',
+        appliance:'',
+        appliance_name: '',
+      });
+    },
+    removeAppliance(id) {
+      if(confirm ('Are you sure?')){
+        this.appliances.splice(id, 1);
+      }
     }
 
-    addRemoveButtonListener(initialItemRow);
-
-    addItemBtn.addEventListener('click', function () {
-        const newItemRow = initialItemRow.cloneNode(true);
-        newItemRow.removeAttribute('id');
-        itemContainer.appendChild(newItemRow);
-        addRemoveButtonListener(newItemRow); 
-    });
-});
-
-
-
+  }))
+})
 </script>
 </html>
--- customer-update.php
+++ customer-update.php
@@ -3,29 +3,32 @@ ob_start();
 session_start();
 include_once 'config.php';
 
-if(isset($_POST['submit'])) {
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     $name = $_POST['name'];
     $type = $_POST['type'];
     $phone = $_POST['phone'];
-    $appliance = $_POST['appliance'];
-    $brand = $_POST['brand'];
-    $appliance_name = $_POST['appliance_name'];
     $address_line_1 = $_POST['address_line_1'];
     $address_line_2 = $_POST['address_line_2'];
     $city = $_POST['city'];
     $gst_no = $_POST['gst_no'] ?? '';
+    $id = $_POST['id'];
 
-    $sql = "UPDATE `customer` SET `name` = '$name', `type` = '$type', `appliance` = '$appliance', `brand` = '$brand',`appliance_name` = '$appliance_name', `phone` = '$phone', `address_line_1` = '$address_line_1', `address_line_2` = '$address_line_2', `city` = '$city', `gst_no` = '$gst_no' WHERE `id` = '$_GET[id]';";
-
+    $sql = "UPDATE `customer` SET `name` = '$name', `type` = '$type', `phone` = '$phone', `address_line_1` = '$address_line_1', `address_line_2` = '$address_line_2', `city` = '$city', `gst_no` = '$gst_no' WHERE `id` = '$id';";
     $result = mysqli_query($con, $sql);
+
+    $delete_items = mysqli_query($con,"DELETE FROM customer_appliances WHERE customer_id = $id");
+
+    foreach($_POST['appliance'] as $key => $value){
+        $appliance = $_POST['appliance'][$key];
+        $brand = $_POST['brand'][$key];
+        $appliance_name = $_POST['appliance_name'][$key];
+        mysqli_query($con,"INSERT INTO customer_appliances(customer_id,appliance,brand,appliance_name) VALUES('$id','$value','$brand','$appliance_name')");
+      }
+
     header("location:customer.php");
     exit;
     
 }
-else {
 
-    echo "<script>alert('Customer not Updated.');</script>";
-    header("location:customer.php");
-}
 ?>
