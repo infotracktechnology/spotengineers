d3200f462406c31c64ad56e43bda333a03521084
infotracktechnology <infotracktechnologysolutions@gmail.com>
2024-11-07 16:44:31 +0530

    sale update

--- bills_create.php
+++ bills_create.php
@@ -13,7 +13,7 @@ $bill_no = $_POST['bill_no'];
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
     if($_POST['search'] == 'Sale') {
         $sale = $con->query("SELECT * FROM `sales` a inner join customer b on a.customer=b.id where a.sale_no=$value and a.cyear = '$cyear'")->fetch_object();
-        $bills = $con->query("INSERT INTO `bills`(`bill_no`, `sale_no`, `customer`, `bill_date`, `bill_type`, `tax_total`, `net_total`, `total`, `cyear`) VALUES ('$bill_no', '$value','$sale->customer','$sale->sale_date', '$sale->sale_type', '$sale->tax_total', '$sale->net_total', '$sale->total', '$sale->cyear')");
+        $bills = $con->query("INSERT INTO `bills`(`bill_no`, `sale_no`, `customer`, `bill_date`, `sale_type`, `tax_total`, `net_total`, `total`, `cyear`) VALUES ('$bill_no', '$value','$sale->customer','$sale->sale_date', '$sale->sale_type', '$sale->tax_total', '$sale->net_total', '$sale->total', '$sale->cyear')");
         $id = $con->insert_id;
         header("Location: sales-print.php?id=$id", true, 303);
         exit;
